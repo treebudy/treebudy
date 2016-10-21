6a4008fbe3153b8ceb98ffe26c44a7ff016e2674
drafter250 <brycevtr250@gmail.com>
2016-10-20 21:18:17 -0400

    added nodes.py

--- tests/test_treebudy.py
+++ tests/test_treebudy.py
@@ -11,20 +11,25 @@ Tests for `treebudy` module.
 import pytest
 
 
-from treebudy import treebudy
+from treebudy.nodes import Snode, Mnode
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+# @pytest.fixture
+# def response():
+#     """Sample pytest fixture.
+#     See more at: http://doc.pytest.org/en/latest/fixture.html
+#     """
+#     import requests
+#     return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument.
-    """
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def test_SNode_init():
+    """Simple Instantiation test for sequence Node type."""
+    root = Snode()
+    assert isinstance(root, Snode)
+
+
+def test_MNode_init():
+    """Simple Instantiation test for mapping Node type."""
+    root = Mnode()
+    assert isinstance(root, Mnode)
